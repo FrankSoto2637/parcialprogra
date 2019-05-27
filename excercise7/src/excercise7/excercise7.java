fcdcd611f5427cf67873eaa7adb7edf8d7bda6ab
FAMILIA <FAMILIA@LAPTOP-1QEVM11H>
2019-05-27 18:25:40 -0500

    third commit

---
+++ excercise7/src/excercise7/excercise7.java
@@ -0,0 +1,26 @@
+package excercise7;
+
+public class excercise7 {
+
+	public static void main(String[] args) {
+		int x=4, w=9;
+		int i1=0;
+		for (int i=-1;i<20;i+=3) {
+			x++;
+			for(int q=4;q<11;q++) {
+				do {
+					i=+3;
+					w=4;
+					i=x+w;
+					x=w+i;
+					i1=i;
+				}while(x<15);
+			}
+		}
+		System.out.println("x:"+x+" "+"i:"+i1);
+		//x=69 e i=65
+	}
+
+	}
+
+
